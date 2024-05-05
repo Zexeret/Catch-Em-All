ef5c30dbc90936a99b2054cde6e12402633cc792
Shyam Rathod <shyam.rathod2601@gmail.com>
2024-05-05 10:22:54 +0530

    feat: player render on map

--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
-import { canvasCtx, initiateCanvas, mapImg } from "./src/core";
+import { canvasCtx, initiateCanvas, mapImg, playerDownImg } from "./src/core";
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./src/utils/constants";
 
 initiateCanvas();
 
 const map_offset = {
-  x: -750,
+  x: -740,
   y: -550,
 };
 
 mapImg.onload = () => {
   canvasCtx.drawImage(mapImg, map_offset.x, map_offset.y);
+
+  canvasCtx.drawImage(
+    playerDownImg,
+    0,
+    0,
+    playerDownImg.width / 4,
+    playerDownImg.height,
+    CANVAS_WIDTH / 2 - playerDownImg.width / 4 / 2,
+    CANVAS_HEIGHT / 2 - playerDownImg.height / 4 / 2,
+    playerDownImg.width / 4,
+    playerDownImg.height
+  );
 };
