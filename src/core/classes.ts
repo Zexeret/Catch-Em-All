c4c75dceb98abdbcc7bfb6e19895002061e69779
Shyam Rathod <shyam.rathod2601@gmail.com>
2024-05-05 15:47:10 +0530

    feat: added collision and blockers

--- src/core/classes.ts
+++ src/core/classes.ts
@@ -1,3 +1,5 @@
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from "../utils/constants";
+import { MovementKeyValues } from "../utils/movementUtils";
 import { canvasCtx } from "./initiateCanvas";
 
 export type Coordinates = {
@@ -5,28 +7,132 @@ export type Coordinates = {
   y: number;
 };
 
-export class Sprite {
+type PlayerSpriteConfig = {
+  frameCount: number;
+};
+
+type BaseSpritePropsType = {
+  position: Coordinates;
+  velocity: number;
+};
+export class BaseSprite {
+  position: Coordinates;
+  velocity: number;
+
+  constructor({ position, velocity }: BaseSpritePropsType) {
+    this.position = { x: position.x, y: position.y };
+    this.velocity = velocity;
+  }
+
+  move(dir: MovementKeyValues) {
+    switch (dir) {
+      case MovementKeyValues.UP:
+        this.#moveUp();
+        break;
+      case MovementKeyValues.DOWN:
+        this.#moveDown();
+        break;
+      case MovementKeyValues.LEFT:
+        this.#moveLeft();
+        break;
+      case MovementKeyValues.RIGHT:
+        this.#moveRight();
+        break;
+    }
+  }
+
+  #moveUp() {
+    this.position.y += this.velocity;
+  }
+  #moveDown() {
+    this.position.y -= this.velocity;
+  }
+  #moveLeft() {
+    this.position.x += this.velocity;
+  }
+  #moveRight() {
+    this.position.x -= this.velocity;
+  }
+}
+
+type SpriteConstructorPropsType = {
   image: HTMLImageElement;
   position: Coordinates;
-  constructor(image: HTMLImageElement, position: Coordinates) {
+  frame?: PlayerSpriteConfig;
+  velocity: number;
+};
+export class Sprite extends BaseSprite {
+  image: HTMLImageElement;
+  frame: PlayerSpriteConfig;
+  width: number;
+  height: number;
+
+  constructor({
+    image,
+    position,
+    velocity,
+    frame = { frameCount: 1 },
+  }: SpriteConstructorPropsType) {
+    super({ position, velocity });
     this.image = image;
-    this.position = position;
+    this.frame = frame;
+
+    this.image.onload = () => {
+      this.width = image.width / frame.frameCount;
+      this.height = image.height;
+    };
   }
 
   draw() {
-    canvasCtx.drawImage(this.image, this.position.x, this.position.y);
+    canvasCtx.drawImage(
+      this.image,
+      0,
+      0,
+      this.width,
+      this.height,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
   }
 
-  moveUp(velocity: number) {
-    this.position.y += velocity;
+  clone() {
+    return new Sprite({
+      image: this.image,
+      position: this.position,
+      velocity: this.velocity,
+      frame: this.frame,
+    });
   }
-  moveDown(velocity: number) {
-    this.position.y -= velocity;
+}
+
+type BoundaryConstructorPropsType = {
+  position: Coordinates;
+  velocity: number;
+};
+export class Boundary extends BaseSprite {
+  static boundaryWidth = 48; // 12px tiles, zoomed in by 4X
+  static boundaryHeight = 48;
+  position: Coordinates;
+  constructor({ position, velocity }: BoundaryConstructorPropsType) {
+    super({ position, velocity });
   }
-  moveLeft(velocity: number) {
-    this.position.x += velocity;
+
+  draw() {
+    canvasCtx.fillStyle = "red";
+    canvasCtx.fillRect(
+      this.position.x,
+      this.position.y,
+      Boundary.boundaryWidth,
+      Boundary.boundaryHeight
+    );
   }
-  moveRight(velocity: number) {
-    this.position.x -= velocity;
+
+  clone() {
+    return new Boundary({
+      position: this.position,
+      velocity: this.velocity,
+    });
   }
 }
--- src/core/startAnimation.ts
+++ src/core/startAnimation.ts
@@ -1,9 +1,10 @@
+import { checkCollision, generateCollisionMap } from "../utils/collisionGrid";
 import { CANVAS_HEIGHT, CANVAS_WIDTH, WALK_VELOCITY } from "../utils/constants";
 import {
   MovementKeyValues,
   lastPressedMovementKey,
 } from "../utils/movementUtils";
-import { Coordinates, Sprite } from "./classes";
+import { BaseSprite, Boundary, Coordinates, Sprite } from "./classes";
 import { canvasCtx } from "./initiateCanvas";
 import { mapImg, playerDownImg } from "./loadSprite";
 import { startEventListeners } from "./startEventListeners";
@@ -11,42 +12,64 @@ import { startEventListeners } from "./startEventListeners";
 export const startAnimation = () => {
   const map_offset: Coordinates = {
     x: -740,
-    y: -550,
+    y: -590,
   };
 
-  const townMap = new Sprite(mapImg, map_offset);
+  const townMap = new Sprite({
+    image: mapImg,
+    position: map_offset,
+    velocity: WALK_VELOCITY,
+  });
+  const playerDownSprite = new Sprite({
+    image: playerDownImg,
+    position: {
+      x: CANVAS_WIDTH / 2 - playerDownImg.width / 4 / 2,
+      y: CANVAS_HEIGHT / 2 - playerDownImg.height / 4 / 2,
+    },
+    frame: {
+      frameCount: 4,
+    },
+    velocity: WALK_VELOCITY,
+  });
+
+  const collisionMapBoundary = generateCollisionMap(map_offset);
+
+  // const testBOundary = new Boundary({ x: 200, y: 400 });
+  // collisionMapBoundary.splice(0, collisionMapBoundary.length);
+  // collisionMapBoundary.push(testBOundary);
 
   startEventListeners();
+
+  const movables = [townMap, ...collisionMapBoundary];
+
+  const renderMovable = (direction: MovementKeyValues) => {
+    let moving = true;
+    for (let i = 0; i < collisionMapBoundary.length; i++) {
+      const futureBoundary = collisionMapBoundary[i].clone();
+      futureBoundary.move(direction);
+      if (checkCollision(playerDownSprite, futureBoundary)) {
+        moving = false;
+        break;
+      }
+    }
+    if (moving) {
+      movables.forEach((movable) => {
+        movable.move(direction);
+      });
+    }
+  };
 
   const animate = () => {
     townMap.draw();
 
-    canvasCtx.drawImage(
-      playerDownImg,
-      0,
-      0,
-      playerDownImg.width / 4,
-      playerDownImg.height,
-      CANVAS_WIDTH / 2 - playerDownImg.width / 4 / 2,
-      CANVAS_HEIGHT / 2 - playerDownImg.height / 4 / 2,
-      playerDownImg.width / 4,
-      playerDownImg.height
-    );
+    playerDownSprite.draw();
 
-    switch (lastPressedMovementKey.slice(-1)[0]) {
-      case MovementKeyValues.DOWN:
-        townMap.moveDown(WALK_VELOCITY);
-        break;
-      case MovementKeyValues.UP:
-        townMap.moveUp(WALK_VELOCITY);
-        break;
-      case MovementKeyValues.LEFT:
-        townMap.moveLeft(WALK_VELOCITY);
-        break;
-      case MovementKeyValues.RIGHT:
-        townMap.moveRight(WALK_VELOCITY);
-        break;
-    }
+    // Uncomment during development only
+    // collisionMapBoundary.forEach((boundary) => {
+    //   boundary.draw();
+    // });
+
+    renderMovable(lastPressedMovementKey.slice(-1)[0]);
 
     window.requestAnimationFrame(animate);
   };
